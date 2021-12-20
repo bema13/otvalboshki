179843c753062c6c183df6cc0faf73ac3b4e8034
bema13 <kurmanbekova_b@auca.kg>
2021-12-20 06:57:07 +0600

    commit bema

--- app/src/main/java/com/example/otvalboshki/MainActivity.kt
+++ app/src/main/java/com/example/otvalboshki/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.otvalboshki
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.otvalboshki.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button2.setOnClickListener {
+            startActivity(Intent()(this, Otvalboshki2::class.java))
+            finish()
+
+            binding.button.setOnClickListener {
+                startActivity(Intent()(this, Otvalboshki3::class.java))
+                finish()
+
+
+        }
+        }
     }
-}
