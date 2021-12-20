615a610132fafaf21bd0f7aff052f56468f0dae8
bema13 <kurmanbekova_b@auca.kg>
2021-12-20 15:18:17 +0600

    commit bema

--- app/src/main/java/com/example/otvalboshki/MainActivity.kt
+++ app/src/main/java/com/example/otvalboshki/MainActivity.kt
@@ -3,25 +3,17 @@ package com.example.otvalboshki
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.otvalboshki.databinding.ActivityMainBinding
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.button2.setOnClickListener {
-            startActivity(Intent()(this, Otvalboshki2::class.java))
-            finish()
-
-            binding.button.setOnClickListener {
-                startActivity(Intent()(this, Otvalboshki3::class.java))
-                finish()
-
-
-        }
-        }
+        setContentView(R.layout.activity_otvalboshki2)
     }
+    val button = findViewById<Button>(R.id.button)
+
+    .setOnClickListener {
+        startActivity(Intent(this, Otvalboshki3::class.java))
+    }
+}
+
--- app/src/main/java/com/example/otvalboshki/Otvalboshki3.kt
+++ app/src/main/java/com/example/otvalboshki/Otvalboshki3.kt
@@ -1,11 +1,18 @@
 package com.example.otvalboshki
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Otvalboshki3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otvalboshki3)
+    }
+    val button1 = findViewById<Button>(R.id.button3)
+
+    .setOnClickListener {
+        startActivity(Intent(this, Otvalboshki5::class.java))
     }
 }
